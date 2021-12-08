64ea5fadb8904befbd7e20a7393aeb5c04ca6f1e
Carlos Estrada <kachiiz@gmail.com>
2021-12-08 13:11:26 -0600

    Adding native lsp configuration

---
+++ nvim/lua/lsp/diagnostic_signs.lua
@@ -0,0 +1,7 @@
+local signs = { Error = " ", Warning = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "LspDiagnosticsSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+
---
+++ nvim/lua/lsp/init.lua
@@ -0,0 +1,4 @@
+require('lsp/nvim-cmp')
+require('lsp/diagnostic_signs')
+require('lsp/language_servers')
+
